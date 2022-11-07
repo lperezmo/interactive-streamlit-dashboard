d859497e704402d1682a59e718e58ddecbd92b90
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2022-11-07 00:19:33 -0600

    Update main.py
    Example form widget

--- main.py
+++ main.py
@@ -508,6 +508,19 @@ st.dataframe(weekly_hrs, use_container_width=True)
 st.subheader("Total hours per day:")
 st.dataframe(daily_hrs, use_container_width=True)
 
+# Example form widget
+st.markdown("#### Example form widget")
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
 
 
 ###########################################################################
